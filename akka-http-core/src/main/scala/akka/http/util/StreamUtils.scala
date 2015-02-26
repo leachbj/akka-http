akka/akka

!str #16902: Unify stream internal representation

akka-http-core/src/main/scala/akka/http/util/StreamUtils.scala
@@ -14,14 +14,11 @@ import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.Try
 import akka.actor.Props
 import akka.http.model.RequestEntity
-import akka.stream.FlowMaterializer
-import akka.stream.impl.Ast.AstNode
-import akka.stream.impl.Ast.StageFactory
+import akka.stream.{ MaterializerSettings, FlowMaterializer, impl }
 import akka.stream.impl.fusing.IteratorInterpreter
 import akka.stream.scaladsl._
 import akka.stream.scaladsl.OperationAttributes._
 import akka.stream.stage._
-import akka.stream.impl
 import akka.util.ByteString
 import org.reactivestreams.{ Subscriber, Publisher }
 
@@ -50,7 +47,7 @@ private[http] object StreamUtils {
   def failedPublisher[T](ex: Throwable): Publisher[T] =
     impl.ErrorPublisher(ex, "failed").asInstanceOf[Publisher[T]]
 
-  def mapErrorTransformer(f: Throwable ⇒ Throwable): Flow[ByteString, ByteString] = {
+  def mapErrorTransformer(f: Throwable ⇒ Throwable): Flow[ByteString, ByteString, Unit] = {
     val transformer = new PushStage[ByteString, ByteString] {
       override def onPush(element: ByteString, ctx: Context[ByteString]): Directive =
         ctx.push(element)
@@ -62,11 +59,12 @@ private[http] object StreamUtils {
     Flow[ByteString].section(name("transformError"))(_.transform(() ⇒ transformer))
   }
 
-  def sliceBytesTransformer(start: Long, length: Long): Flow[ByteString, ByteString] = {
+  def sliceBytesTransformer(start: Long, length: Long): Flow[ByteString, ByteString, Unit] = {
     val transformer = new StatefulStage[ByteString, ByteString] {
 
       def skipping = new State {
         var toSkip = start
+
         override def onPush(element: ByteString, ctx: Context[ByteString]): Directive =
           if (element.length < toSkip) {
             // keep skipping
@@ -78,8 +76,10 @@ private[http] object StreamUtils {
             current.onPush(element.drop(toSkip.toInt), ctx)
           }
       }
+
       def taking(initiallyRemaining: Long) = new State {
         var remaining: Long = initiallyRemaining
+
         override def onPush(element: ByteString, ctx: Context[ByteString]): Directive = {
           val data = element.take(math.min(remaining, Int.MaxValue).toInt)
           remaining -= data.size
@@ -93,9 +93,10 @@ private[http] object StreamUtils {
     Flow[ByteString].section(name("sliceBytes"))(_.transform(() ⇒ transformer))
   }
 
-  def limitByteChunksStage(maxBytesPerChunk: Int): Stage[ByteString, ByteString] =
+  def limitByteChunksStage(maxBytesPerChunk: Int): PushPullStage[ByteString, ByteString] =
     new StatefulStage[ByteString, ByteString] {
       def initial = WaitingForData
+
       case object WaitingForData extends State {
         def onPush(elem: ByteString, ctx: Context[ByteString]): Directive =
           if (elem.size <= maxBytesPerChunk) ctx.push(elem)
@@ -104,6 +105,7 @@ private[http] object StreamUtils {
             ctx.push(elem.take(maxBytesPerChunk))
           }
       }
+
       case class DeliveringData(remaining: ByteString) extends State {
         def onPush(elem: ByteString, ctx: Context[ByteString]): Directive =
           throw new IllegalStateException("Not expecting data")
@@ -132,23 +134,21 @@ private[http] object StreamUtils {
    * Applies a sequence of transformers on one source and returns a sequence of sources with the result. The input source
    * will only be traversed once.
    */
-  def transformMultiple(input: Source[ByteString], transformers: immutable.Seq[Flow[ByteString, ByteString]])(implicit materializer: FlowMaterializer): immutable.Seq[Source[ByteString]] =
+  def transformMultiple(input: Source[ByteString, Unit], transformers: immutable.Seq[Flow[ByteString, ByteString, _]])(implicit materializer: FlowMaterializer): immutable.Seq[Source[ByteString, Unit]] =
     transformers match {
       case Nil      ⇒ Nil
       case Seq(one) ⇒ Vector(input.via(one))
       case multiple ⇒
-        val results = Vector.fill(multiple.size)(Sink.publisher[ByteString])
-        val mat =
-          FlowGraph { implicit b ⇒
-            import FlowGraphImplicits._
+        val (fanoutSub, fanoutPub) = Source.subscriber[ByteString]().toMat(Sink.fanoutPublisher(16, 16))(Pair.apply).run()
+        val sources = transformers.map { flow ⇒
+          // Doubly wrap to ensure that subscription to the running publisher happens before the final sources
+          // are exposed, so there is no race
+          Source(Source(fanoutPub).via(flow).runWith(Sink.publisher()))
+        }
+        // The fanout publisher must be wired to the original source after all fanout subscribers have been subscribed
+        input.runWith(Sink(fanoutSub))
+        sources
 
-            val broadcast = Broadcast[ByteString](OperationAttributes.name("transformMultipleInputBroadcast"))
-            input ~> broadcast
-            (multiple, results).zipped.foreach { (trans, sink) ⇒
-              broadcast ~> trans ~> sink
-            }
-          }.run()
-        results.map(s ⇒ Source(mat.get(s)))
     }
 
   def mapEntityError(f: Throwable ⇒ Throwable): RequestEntity ⇒ RequestEntity =
@@ -159,116 +159,53 @@ private[http] object StreamUtils {
    *
    * FIXME: should be provided by akka-stream, see #15588
    */
-  def fromInputStreamSource(inputStream: InputStream, defaultChunkSize: Int = 65536): Source[ByteString] = {
+  def fromInputStreamSource(inputStream: InputStream,
+                            fileIODispatcher: String,
+                            defaultChunkSize: Int = 65536): Source[ByteString, Unit] = {
     import akka.stream.impl._
 
-    def props(materializer: ActorBasedFlowMaterializer): Props = {
-      val iterator = new Iterator[ByteString] {
-        var finished = false
-        def hasNext: Boolean = !finished
-        def next(): ByteString =
-          if (!finished) {
-            val buffer = new Array[Byte](defaultChunkSize)
-            val read = inputStream.read(buffer)
-            if (read < 0) {
-              finished = true
-              inputStream.close()
-              ByteString.empty
-            } else ByteString.fromArray(buffer, 0, read)
-          } else ByteString.empty
-      }
+    val onlyOnceFlag = new AtomicBoolean(false)
 
-      IteratorPublisher.props(iterator, materializer.settings).withDispatcher(materializer.settings.fileIODispatcher)
+    val iterator = new Iterator[ByteString] {
+      var finished = false
+      if (onlyOnceFlag.get() || !onlyOnceFlag.compareAndSet(false, true))
+        throw new IllegalStateException("One time source can only be instantiated once")
+
+      def hasNext: Boolean = !finished
+
+      def next(): ByteString =
+        if (!finished) {
+          val buffer = new Array[Byte](defaultChunkSize)
+          val read = inputStream.read(buffer)
+          if (read < 0) {
+            finished = true
+            inputStream.close()
+            ByteString.empty
+          } else ByteString.fromArray(buffer, 0, read)
+        } else ByteString.empty
     }
 
-    new AtomicBoolean(false) with SimpleActorFlowSource[ByteString] {
-      override def attach(flowSubscriber: Subscriber[ByteString], materializer: ActorBasedFlowMaterializer, flowName: String): Unit =
-        create(materializer, flowName)._1.subscribe(flowSubscriber)
+    Source(() ⇒ iterator).withAttributes(OperationAttributes.dispatcher(fileIODispatcher))
 
-      override def isActive: Boolean = true
-      override def create(materializer: ActorBasedFlowMaterializer, flowName: String): (Publisher[ByteString], Unit) =
-        if (!getAndSet(true)) {
-          val ref = materializer.actorOf(props(materializer), name = s"$flowName-0-InputStream-source")
-          val publisher = ActorPublisher[ByteString](ref)
-          ref ! ExposedPublisher(publisher.asInstanceOf[impl.ActorPublisher[Any]])
-
-          (publisher, ())
-        } else (ErrorPublisher(new IllegalStateException("One time source can only be instantiated once"), "failed").asInstanceOf[Publisher[ByteString]], ())
-    }
   }
 
   /**
    * Returns a source that can only be used once for testing purposes.
    */
-  def oneTimeSource[T](other: Source[T]): Source[T] = {
-    import akka.stream.impl._
-    val original = other.asInstanceOf[ActorFlowSource[T]]
-    new AtomicBoolean(false) with SimpleActorFlowSource[T] {
-      override def attach(flowSubscriber: Subscriber[T], materializer: ActorBasedFlowMaterializer, flowName: String): Unit =
-        create(materializer, flowName)._1.subscribe(flowSubscriber)
-      override def isActive: Boolean = true
-      override def create(materializer: ActorBasedFlowMaterializer, flowName: String): (Publisher[T], Unit) =
-        if (!getAndSet(true)) (original.create(materializer, flowName)._1, ())
-        else (ErrorPublisher(new IllegalStateException("One time source can only be instantiated once"), "failed").asInstanceOf[Publisher[T]], ())
+  def oneTimeSource[T, Mat](other: Source[T, Mat]): Source[T, Mat] = {
+    val onlyOnceFlag = new AtomicBoolean(false)
+    other.map { elem ⇒
+      if (onlyOnceFlag.get() || !onlyOnceFlag.compareAndSet(false, true))
+        throw new IllegalStateException("One time source can only be instantiated once")
+      elem
     }
   }
-
-  def runStrict(sourceData: ByteString, transformer: Flow[ByteString, ByteString], maxByteSize: Long, maxElements: Int): Try[Option[ByteString]] =
-    runStrict(Iterator.single(sourceData), transformer, maxByteSize, maxElements)
-
-  def runStrict(sourceData: Iterator[ByteString], transformer: Flow[ByteString, ByteString], maxByteSize: Long, maxElements: Int): Try[Option[ByteString]] =
-    Try {
-      transformer match {
-        // FIXME #16382 right now the flow can't use keys, should that be allowed?
-        case Pipe(ops, keys, _) if keys.isEmpty ⇒
-          if (ops.isEmpty)
-            Some(sourceData.foldLeft(ByteString.empty)(_ ++ _))
-          else {
-            @tailrec def tryBuild(remaining: List[AstNode], acc: List[PushPullStage[ByteString, ByteString]]): List[PushPullStage[ByteString, ByteString]] =
-              remaining match {
-                case Nil ⇒ acc.reverse
-                case StageFactory(mkStage, _) :: tail ⇒
-                  mkStage() match {
-                    case d: PushPullStage[ByteString, ByteString] ⇒
-                      tryBuild(tail, d :: acc)
-                    case _ ⇒ Nil
-                  }
-                case _ ⇒ Nil
-              }
-
-            val strictOps = tryBuild(ops, Nil)
-            if (strictOps.isEmpty)
-              None
-            else {
-              val iter: Iterator[ByteString] = new IteratorInterpreter(sourceData, strictOps).iterator
-              var byteSize = 0L
-              var result = ByteString.empty
-              var i = 0
-              // note that iter.next() will throw exception if the stream fails, caught by the enclosing Try
-              while (iter.hasNext) {
-                i += 1
-                if (i > maxElements)
-                  throw new IllegalArgumentException(s"Too many elements produced by byte transformation, $i was greater than max allowed $maxElements elements")
-                val elem = iter.next()
-                byteSize += elem.size
-                if (byteSize > maxByteSize)
-                  throw new IllegalArgumentException(s"Too large data result, $byteSize bytes was greater than max allowed $maxByteSize bytes")
-                result ++= elem
-              }
-              Some(result)
-            }
-          }
-
-        case _ ⇒ None
-      }
-    }
-
 }
 
 /**
  * INTERNAL API
  */
-private[http] class EnhancedByteStringSource(val byteStringStream: Source[ByteString]) extends AnyVal {
+private[http] class EnhancedByteStringSource[Mat](val byteStringStream: Source[ByteString, Mat]) extends AnyVal {
   def join(implicit materializer: FlowMaterializer): Future[ByteString] =
     byteStringStream.runFold(ByteString.empty)(_ ++ _)
   def utf8String(implicit materializer: FlowMaterializer, ec: ExecutionContext): Future[String] =

leachbj/akka-http

 also =str #16912: Fix StreamTcpSpec flakiness

akka-http-core/src/main/scala/akka/http/util/StreamUtils.scala
@@ -14,15 +14,11 @@ import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.Try
 import akka.actor.Props
 import akka.http.model.RequestEntity
-import akka.stream.ActorFlowMaterializer
-import akka.stream.FlowMaterializer
-import akka.stream.impl.Ast.AstNode
-import akka.stream.impl.Ast.StageFactory
+import akka.stream.{ ActorFlowMaterializerSettings, ActorFlowMaterializer, impl }
 import akka.stream.impl.fusing.IteratorInterpreter
 import akka.stream.scaladsl._
 import akka.stream.scaladsl.OperationAttributes._
 import akka.stream.stage._
-import akka.stream.impl
 import akka.util.ByteString
 import org.reactivestreams.{ Subscriber, Publisher }
 
@@ -51,7 +47,7 @@ private[http] object StreamUtils {
   def failedPublisher[T](ex: Throwable): Publisher[T] =
     impl.ErrorPublisher(ex, "failed").asInstanceOf[Publisher[T]]
 
-  def mapErrorTransformer(f: Throwable ⇒ Throwable): Flow[ByteString, ByteString] = {
+  def mapErrorTransformer(f: Throwable ⇒ Throwable): Flow[ByteString, ByteString, Unit] = {
     val transformer = new PushStage[ByteString, ByteString] {
       override def onPush(element: ByteString, ctx: Context[ByteString]): Directive =
         ctx.push(element)
@@ -63,11 +59,12 @@ private[http] object StreamUtils {
     Flow[ByteString].section(name("transformError"))(_.transform(() ⇒ transformer))
   }
 
-  def sliceBytesTransformer(start: Long, length: Long): Flow[ByteString, ByteString] = {
+  def sliceBytesTransformer(start: Long, length: Long): Flow[ByteString, ByteString, Unit] = {
     val transformer = new StatefulStage[ByteString, ByteString] {
 
       def skipping = new State {
         var toSkip = start
+
         override def onPush(element: ByteString, ctx: Context[ByteString]): Directive =
           if (element.length < toSkip) {
             // keep skipping
@@ -79,8 +76,10 @@ private[http] object StreamUtils {
             current.onPush(element.drop(toSkip.toInt), ctx)
           }
       }
+
       def taking(initiallyRemaining: Long) = new State {
         var remaining: Long = initiallyRemaining
+
         override def onPush(element: ByteString, ctx: Context[ByteString]): Directive = {
           val data = element.take(math.min(remaining, Int.MaxValue).toInt)
           remaining -= data.size
@@ -94,9 +93,10 @@ private[http] object StreamUtils {
     Flow[ByteString].section(name("sliceBytes"))(_.transform(() ⇒ transformer))
   }
 
-  def limitByteChunksStage(maxBytesPerChunk: Int): Stage[ByteString, ByteString] =
+  def limitByteChunksStage(maxBytesPerChunk: Int): PushPullStage[ByteString, ByteString] =
     new StatefulStage[ByteString, ByteString] {
       def initial = WaitingForData
+
       case object WaitingForData extends State {
         def onPush(elem: ByteString, ctx: Context[ByteString]): Directive =
           if (elem.size <= maxBytesPerChunk) ctx.push(elem)
@@ -105,6 +105,7 @@ private[http] object StreamUtils {
             ctx.push(elem.take(maxBytesPerChunk))
           }
       }
+
       case class DeliveringData(remaining: ByteString) extends State {
         def onPush(elem: ByteString, ctx: Context[ByteString]): Directive =
           throw new IllegalStateException("Not expecting data")
@@ -133,23 +134,21 @@ private[http] object StreamUtils {
    * Applies a sequence of transformers on one source and returns a sequence of sources with the result. The input source
    * will only be traversed once.
    */
-  def transformMultiple(input: Source[ByteString], transformers: immutable.Seq[Flow[ByteString, ByteString]])(implicit materializer: FlowMaterializer): immutable.Seq[Source[ByteString]] =
+  def transformMultiple(input: Source[ByteString, Unit], transformers: immutable.Seq[Flow[ByteString, ByteString, _]])(implicit materializer: ActorFlowMaterializer): immutable.Seq[Source[ByteString, Unit]] =
     transformers match {
       case Nil      ⇒ Nil
       case Seq(one) ⇒ Vector(input.via(one))
       case multiple ⇒
-        val results = Vector.fill(multiple.size)(Sink.publisher[ByteString])
-        val mat =
-          FlowGraph { implicit b ⇒
-            import FlowGraphImplicits._
+        val (fanoutSub, fanoutPub) = Source.subscriber[ByteString]().toMat(Sink.fanoutPublisher(16, 16))(Keep.both).run()
+        val sources = transformers.map { flow ⇒
+          // Doubly wrap to ensure that subscription to the running publisher happens before the final sources
+          // are exposed, so there is no race
+          Source(Source(fanoutPub).via(flow).runWith(Sink.publisher()))
+        }
+        // The fanout publisher must be wired to the original source after all fanout subscribers have been subscribed
+        input.runWith(Sink(fanoutSub))
+        sources
 
-            val broadcast = Broadcast[ByteString](OperationAttributes.name("transformMultipleInputBroadcast"))
-            input ~> broadcast
-            (multiple, results).zipped.foreach { (trans, sink) ⇒
-              broadcast ~> trans ~> sink
-            }
-          }.run()
-        results.map(s ⇒ Source(mat.get(s)))
     }
 
   def mapEntityError(f: Throwable ⇒ Throwable): RequestEntity ⇒ RequestEntity =
@@ -160,118 +159,55 @@ private[http] object StreamUtils {
    *
    * FIXME: should be provided by akka-stream, see #15588
    */
-  def fromInputStreamSource(inputStream: InputStream, defaultChunkSize: Int = 65536): Source[ByteString] = {
+  def fromInputStreamSource(inputStream: InputStream,
+                            fileIODispatcher: String,
+                            defaultChunkSize: Int = 65536): Source[ByteString, Unit] = {
     import akka.stream.impl._
 
-    def props(materializer: ActorFlowMaterializer): Props = {
-      val iterator = new Iterator[ByteString] {
-        var finished = false
-        def hasNext: Boolean = !finished
-        def next(): ByteString =
-          if (!finished) {
-            val buffer = new Array[Byte](defaultChunkSize)
-            val read = inputStream.read(buffer)
-            if (read < 0) {
-              finished = true
-              inputStream.close()
-              ByteString.empty
-            } else ByteString.fromArray(buffer, 0, read)
-          } else ByteString.empty
-      }
+    val onlyOnceFlag = new AtomicBoolean(false)
 
-      IteratorPublisher.props(iterator, materializer.settings).withDispatcher(materializer.settings.fileIODispatcher)
+    val iterator = new Iterator[ByteString] {
+      var finished = false
+      if (onlyOnceFlag.get() || !onlyOnceFlag.compareAndSet(false, true))
+        throw new IllegalStateException("One time source can only be instantiated once")
+
+      def hasNext: Boolean = !finished
+
+      def next(): ByteString =
+        if (!finished) {
+          val buffer = new Array[Byte](defaultChunkSize)
+          val read = inputStream.read(buffer)
+          if (read < 0) {
+            finished = true
+            inputStream.close()
+            ByteString.empty
+          } else ByteString.fromArray(buffer, 0, read)
+        } else ByteString.empty
     }
 
-    new AtomicBoolean(false) with SimpleActorFlowSource[ByteString] {
-      override def attach(flowSubscriber: Subscriber[ByteString], materializer: ActorFlowMaterializer, flowName: String): Unit =
-        create(materializer, flowName)._1.subscribe(flowSubscriber)
+    Source(() ⇒ iterator).withAttributes(OperationAttributes.dispatcher(fileIODispatcher))
 
-      override def isActive: Boolean = true
-      override def create(materializer: ActorFlowMaterializer, flowName: String): (Publisher[ByteString], Unit) =
-        if (!getAndSet(true)) {
-          val ref = materializer.actorOf(props(materializer), name = s"$flowName-0-InputStream-source")
-          val publisher = ActorPublisher[ByteString](ref)
-          ref ! ExposedPublisher(publisher.asInstanceOf[impl.ActorPublisher[Any]])
-
-          (publisher, ())
-        } else (ErrorPublisher(new IllegalStateException("One time source can only be instantiated once"), "failed").asInstanceOf[Publisher[ByteString]], ())
-    }
   }
 
   /**
    * Returns a source that can only be used once for testing purposes.
    */
-  def oneTimeSource[T](other: Source[T]): Source[T] = {
-    import akka.stream.impl._
-    val original = other.asInstanceOf[ActorFlowSource[T]]
-    new AtomicBoolean(false) with SimpleActorFlowSource[T] {
-      override def attach(flowSubscriber: Subscriber[T], materializer: ActorFlowMaterializer, flowName: String): Unit =
-        create(materializer, flowName)._1.subscribe(flowSubscriber)
-      override def isActive: Boolean = true
-      override def create(materializer: ActorFlowMaterializer, flowName: String): (Publisher[T], Unit) =
-        if (!getAndSet(true)) (original.create(materializer, flowName)._1, ())
-        else (ErrorPublisher(new IllegalStateException("One time source can only be instantiated once"), "failed").asInstanceOf[Publisher[T]], ())
+  def oneTimeSource[T, Mat](other: Source[T, Mat]): Source[T, Mat] = {
+    val onlyOnceFlag = new AtomicBoolean(false)
+    other.map { elem ⇒
+      if (onlyOnceFlag.get() || !onlyOnceFlag.compareAndSet(false, true))
+        throw new IllegalStateException("One time source can only be instantiated once")
+      elem
     }
   }
-
-  def runStrict(sourceData: ByteString, transformer: Flow[ByteString, ByteString], maxByteSize: Long, maxElements: Int): Try[Option[ByteString]] =
-    runStrict(Iterator.single(sourceData), transformer, maxByteSize, maxElements)
-
-  def runStrict(sourceData: Iterator[ByteString], transformer: Flow[ByteString, ByteString], maxByteSize: Long, maxElements: Int): Try[Option[ByteString]] =
-    Try {
-      transformer match {
-        // FIXME #16382 right now the flow can't use keys, should that be allowed?
-        case Pipe(ops, keys, _) if keys.isEmpty ⇒
-          if (ops.isEmpty)
-            Some(sourceData.foldLeft(ByteString.empty)(_ ++ _))
-          else {
-            @tailrec def tryBuild(remaining: List[AstNode], acc: List[PushPullStage[ByteString, ByteString]]): List[PushPullStage[ByteString, ByteString]] =
-              remaining match {
-                case Nil ⇒ acc.reverse
-                case StageFactory(mkStage, _) :: tail ⇒
-                  mkStage() match {
-                    case d: PushPullStage[ByteString, ByteString] ⇒
-                      tryBuild(tail, d :: acc)
-                    case _ ⇒ Nil
-                  }
-                case _ ⇒ Nil
-              }
-
-            val strictOps = tryBuild(ops, Nil)
-            if (strictOps.isEmpty)
-              None
-            else {
-              val iter: Iterator[ByteString] = new IteratorInterpreter(sourceData, strictOps).iterator
-              var byteSize = 0L
-              var result = ByteString.empty
-              var i = 0
-              // note that iter.next() will throw exception if the stream fails, caught by the enclosing Try
-              while (iter.hasNext) {
-                i += 1
-                if (i > maxElements)
-                  throw new IllegalArgumentException(s"Too many elements produced by byte transformation, $i was greater than max allowed $maxElements elements")
-                val elem = iter.next()
-                byteSize += elem.size
-                if (byteSize > maxByteSize)
-                  throw new IllegalArgumentException(s"Too large data result, $byteSize bytes was greater than max allowed $maxByteSize bytes")
-                result ++= elem
-              }
-              Some(result)
-            }
-          }
-
-        case _ ⇒ None
-      }
-    }
-
 }
 
 /**
  * INTERNAL API
  */
-private[http] class EnhancedByteStringSource(val byteStringStream: Source[ByteString]) extends AnyVal {
-  def join(implicit materializer: FlowMaterializer): Future[ByteString] =
+private[http] class EnhancedByteStringSource[Mat](val byteStringStream: Source[ByteString, Mat]) extends AnyVal {
+  def join(implicit materializer: ActorFlowMaterializer): Future[ByteString] =
     byteStringStream.runFold(ByteString.empty)(_ ++ _)
-  def utf8String(implicit materializer: FlowMaterializer, ec: ExecutionContext): Future[String] =
+  def utf8String(implicit materializer: ActorFlowMaterializer, ec: ExecutionContext): Future[String] =
     join.map(_.utf8String)
 }