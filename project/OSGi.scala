papayamomo/akka

!clt #13711 Move cluster tools from contrib
* new module akka-cluster-tools containing
  cluster singleton, distributed pub-sub, and cluster client

project/OSGi.scala
@@ -27,6 +27,9 @@ object OSGi {
   val camel = exports(Seq("akka.camel.*"))
 
   val cluster = exports(Seq("akka.cluster.*"), imports = Seq(protobufImport()))
+  
+  val clusterTools = exports(Seq("akka.cluster.client.*", "akka.cluster.pubsub.*", "akka.cluster.pubsub.*"), 
+      imports = Seq(protobufImport()))
 
   val clusterMetrics = exports(Seq("akka.cluster.metrics.*"), imports = Seq(protobufImport(),kamonImport(),sigarImport()))
 

leachbj/akka-http

project/OSGi.scala
@@ -27,6 +27,9 @@ object OSGi {
   val camel = exports(Seq("akka.camel.*"))
 
   val cluster = exports(Seq("akka.cluster.*"), imports = Seq(protobufImport()))
+  
+  val clusterTools = exports(Seq("akka.cluster.singleton.*", "akka.cluster.client.*", "akka.cluster.pubsub.*"), 
+      imports = Seq(protobufImport()))
 
   val clusterMetrics = exports(Seq("akka.cluster.metrics.*"), imports = Seq(protobufImport(),kamonImport(),sigarImport()))
 